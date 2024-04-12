a9c7021fb0448e9572d68b98db0a9080f5fb1276
Bernat Domokos <domberills@gmail.com>
2024-04-12 18:20:00 +0300

    tailwindcss update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline">About page with tailwind!</h1>
+			<h1 className="text-3xl font-bold underline">About page with tailwindcss!</h1>
 		</div>
 	);
 }
