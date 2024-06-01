71fe4e7a4a02df1f6d12925943552e8ee44d4254
Bernat Domokos <domberills@gmail.com>
2024-06-01 11:42:55 +0300

    before first aws test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import About from "@/pages/about";
+import { Button } from "@mui/material";
 
 export default function Home() {
-	return (
-		<main>
-			<div>Start creating now...</div>
-
-			<About />
-		</main>
-	);
+  return (
+    <main>
+      <div>Start creating now...</div>
+      {/* <Button>Login</Button> */}
+      <About />
+    </main>
+  );
 }
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 export default function About() {
-	return (
-		<div>
-			<h1 className="text-3xl font-bold underline">About page with tailwindcss!</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">About page</h1>
+    </div>
+  );
 }
