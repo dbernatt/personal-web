9f2544424125a99ac4ec5f9086120a426839bf16
Bernat Domokos <domberills@gmail.com>
2024-06-07 23:49:34 +0300

    ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,19 +15,21 @@ export default function Home() {
       <div className="align-middle justify-center content-center text-center">
         <div className="my-4 text-4xl">Hi, I'm Bernat</div>
         <div className="my-4 text-2xl">A software developer</div>
-        <div className="my-4">coming soon... </div>
-        <hr className="border-gray-700 mx-9 " />
+        <div className="my-10">coming soon... </div>
+        <hr className="border-gray-600 border-1 my-10" />
         <div className="mx-2 text-xl align-middle justify-center content-center">
-          <p className="my-6">While you wait, check this person who does not exist:</p>
+          <p className="my-5">While you wait, check this person who does not exist:</p>
+          <Button size="large" color="primary" startIcon={<Refresh />} onClick={handleRegenerate}>
+            Regenerate
+          </Button>
+          <br />
+          <br />
           <img
             className="ml-auto mr-auto block w-min rounded-lg"
             alt="This person does not exist"
             src={imageSrc}
           />
           <br />
-          <Button size="large" color="primary" startIcon={<Refresh />} onClick={handleRegenerate}>
-            Regenerate
-          </Button>
 
           <br />
           <br />
